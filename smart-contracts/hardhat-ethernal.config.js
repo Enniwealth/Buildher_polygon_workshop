995fa7e48304bfb8a9c4aeef23602446b014dd0f
enniwealth <wealththefirstx1000@gmail.com>
2024-06-14 18:32:17 +0100

    changed network url

--- smart-contracts/hardhat-ethernal.config.js
+++ smart-contracts/hardhat-ethernal.config.js
@@ -23,46 +23,6 @@ const config = {
     hardhat: {
       loggingEnabled: false
     },
-    ethereum: {
-      chainId: 1,
-      url: "https://rpc.ankr.com/eth",
-      forking: {
-        url: "https://rpc.ankr.com/eth"
-      },
-      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
-    },
-    sepolia: {
-      chainId: 11155111,
-      url: "https://rpc.ankr.com/eth_sepolia",
-      forking: {
-        url: "https://rpc.ankr.com/eth_sepolia"
-      },
-      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
-    },
-    polygonPos: {
-      chainId: 137,
-      url: "https://rpc.ankr.com/polygon",
-      forking: {
-        url: "https://rpc.ankr.com/polygon"
-      },
-      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
-    },
-    polygonAmoy: {
-      chainId: 80002,
-      url: "https://rpc-amoy.polygon.technology",
-      forking: {
-        url: "https://rpc-amoy.polygon.technology"
-      },
-      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
-    },
-    polygonZkevm: {
-      chainId: 1101,
-      url: "https://rpc.ankr.com/polygon_zkevm",
-      forking: {
-        url: "https://rpc.ankr.com/polygon_zkevm"
-      },
-      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
-    },
     polygonZkevmTestnet: {
       chainId: 2442,
       url: "https://rpc.cardona.zkevm-rpc.com",
